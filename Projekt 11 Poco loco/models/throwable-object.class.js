1d1faa1b299e781a5f1dee2a53130b35c54aa8b9
Ioannis Karakasidis <168634993+Ioannis-Karakasidis@users.noreply.github.com>
2024-12-26 13:22:22 +0100

    Update throwable-object.class.js

--- Projekt 11 Poco loco/models/throwable-object.class.js
+++ Projekt 11 Poco loco/models/throwable-object.class.js
@@ -10,7 +10,7 @@ class ThrowableObject extends MovableObject {
   }
 
   throw() {
-    this.speedY = 10;
+this.speedY = 1                                                                                                                                                                                                                                      °^^^^  Q0;
     this.applyGravity();
     setInterval(() => {
       this.x += 10;
